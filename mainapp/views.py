147fa5f01d4163bd3f4e2ebdd47d8920240d17c1
Nikita Madorsky <madorskii@mail.ru>
2021-02-09 14:31:20 +0300

    Lesson 4. Data from files

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,19 +2,19 @@ from django.conf import settings
 from django.shortcuts import render
 from django.utils import timezone
 
-from .models import Product, ProductCategory
+from .models import Contact, Product, ProductCategory
 
 
 def main(request):
     title = "главная"
 
-    products = Product.objects.all()
+    products = Product.objects.all()[:4]
 
     content = {"title": title, "products": products, "media_url": settings.MEDIA_URL}
     return render(request, "mainapp/index.html", content)
 
 
-def products(request):
+def products(request, pk=None):
     title = "продукты"
     links_menu = ProductCategory.objects.all()
     same_products = Product.objects.all()
@@ -24,26 +24,15 @@ def products(request):
         "same_products": same_products,
         "media_url": settings.MEDIA_URL,
     }
+    if pk:
+        print(f"User select category: {pk}")
     return render(request, "mainapp/products.html", content)
 
 
 def contact(request):
     title = "о нас"
     visit_date = timezone.now()
-    locations = [
-        {"city": "Москва", "phone": "+7-888-888-8888", "email": "info@geekshop.ru", "address": "В пределах МКАД"},
-        {
-            "city": "Екатеринбург",
-            "phone": "+7-777-777-7777",
-            "email": "info_yekaterinburg@geekshop.ru",
-            "address": "Близко к центру",
-        },
-        {
-            "city": "Владивосток",
-            "phone": "+7-999-999-9999",
-            "email": "info_vladivostok@geekshop.ru",
-            "address": "Близко к океану",
-        },
-    ]
+    locations = Contact.objects.all()
     content = {"title": title, "visit_date": visit_date, "locations": locations}
     return render(request, "mainapp/contact.html", content)
+    
